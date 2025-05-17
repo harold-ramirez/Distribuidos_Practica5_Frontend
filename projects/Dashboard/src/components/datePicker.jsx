b2d3e5fe616d28012c638b322a2563ac1e5206a0
Harold-Ramirez <ramirezpereiraharold@gmail.com>
2025-05-17 13:17:25 -0400

    feat: ✨ Enhance DatePicker component with date selection functionality

--- projects/Dashboard/src/components/datePicker.jsx
+++ projects/Dashboard/src/components/datePicker.jsx
@@ -1,6 +1,29 @@
-export default function DatePicker(){
+import { useState } from "react";
+
+export default function DatePicker() {
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
 
   return (
-    <input type="date" className="border border-black rounded-lg text-black h-10 p-1" />
-  )
+    <div className="flex flex-row gap-4">
+      <div>
+        <label>Desde:</label>
+        <input
+          type="date"
+          className="bg-gray-700 ml-1 px-2 border rounded-lg h-10 text-white"
+          value={startDate}
+          onChange={(e) => setStartDate(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Hasta:</label>
+        <input
+          type="date"
+          className="bg-gray-700 ml-1 px-2 border rounded-lg h-10 text-white"
+          value={endDate}
+          onChange={(e) => setEndDate(e.target.value)}
+        />
+      </div>
+    </div>
+  );
 }
